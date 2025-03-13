577d913ca07f5f98a203997e44c4f3ed749a8849
Nii Boye <saint.niiboye@gmail.com>
2025-03-13 09:53:15 +0000

    Create contact section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import { Landing, Navbar, OurServices, Testimonials } from "../components"
+import Contacts from "./Contacts"
 
 let App = () => {
     return (<>
@@ -13,6 +14,7 @@ let App = () => {
             <Testimonials />
 
             {/* Get in touch */}
+            <Contacts />
         </div>
     </>)
 }
