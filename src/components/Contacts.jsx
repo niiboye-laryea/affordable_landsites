172053f0f416ab12635f2c3631b11529ad8a22c9
Nii Boye <saint.niiboye@gmail.com>
2025-03-13 20:19:02 +0000

    Update on the contact section

--- src/components/Contacts.jsx
+++ src/components/Contacts.jsx
@@ -1,23 +1,38 @@
-import { FaBeer, FaFacebook, FaFacebookF, FaInstagram, FaTwitter } from "react-icons/fa"
+import { FaBeer, FaFacebook, FaFacebookF, FaInstagram, FaPhone, FaPhoneAlt, FaPhoneSlash, FaTwitter } from "react-icons/fa"
 import React from "react"
 
 const Contacts = () => {
     return (<>
         <div id="contact_us" className="pt-20 pb-20 px-10 bg-slate-800 text-slate-400 text-center">
-            <p className="text-5xl font-bold text-slate-300">Get In Touch With Us</p>
-            <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 mt-10">
+            <p className="text-left lg:text-center text-5xl font-bold text-slate-300">Get In Touch With Us</p>
+            <div className="grid grid-cols-1 lg:grid-cols-2 gap-10 mt-15">
                 <div className="text-left text-[18px]">
-                    <p>Shanton Peaceful Home</p>
-                    <p>Post Office Box 34KL</p>
-                    <p>Kuntami 04 street,</p> 
-                    <p>Kumansi, Ghana</p>
-                    <a href="#">
-                        <p className="mt-5 flex items-center gap-2">
-                            <FaTwitter /> shantonpeaceful
+                    <div className="flex flex-col">
+                        <span>Shanton Peaceful Home</span>
+                        <span>Post Office Box 34KL</span>
+                        <span>Kuntami 04 street,</span> 
+                        <span>Kumansi, Ghana</span>
+                    </div>
+                    <div>
+                        <a href="#" className="social-links">
+                            <span className="mt-4 flex items-center gap-4"><FaTwitter /> shantshome.peaceful  </span>
+                        </a>
+                        <a href="#" className="social-links">
+                            <span className="mt-2 flex items-center gap-4"><FaFacebook /> shantshome</span>
+                        </a>
+                        <a href="#" className="social-links">
+                            <span className="mt-2 flex items-center gap-4"><FaInstagram /> shantonspeaceful</span>
+                        </a>
+                    </div>
+                    <div>
+                        <p className="mt-4 flex items-center gap-4">
+                            <FaPhoneAlt /> 
+                            <section className="flex flex-col">
+                                <p>+241 215 885442</p>
+                                <p>+233 552 421585</p>
+                            </section>
                         </p>
-                    </a>
-                    <p className="mt-2 flex items-center gap-2"><FaFacebook /> shantonpeaceful</p>
-                    <p className="mt-2 flex items-center gap-2"><FaInstagram /> shantonpeaceful</p>
+                    </div>
                 </div>
                 <div className="text-left text-[18px]">adsf</div>
             </div>
